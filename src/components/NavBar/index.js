e12d53515aee86495a3c4e6406f3dd74c3cf7e2e
joselitolima21 <joselitolima90@hotmail.com>
2020-10-17 21:27:10 -0300

    #1 - Design inicial da NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './styles.css'
+import drop from '../../images/drop-down.svg'
 
 function NavBar() {
-  return <div className='nav-bar'>oi</div>;
+  return (
+    <div className='nav-bar'>
+      <h1>QR Code Generator</h1>
+      <button className='white-button'>
+        <h2>English</h2>
+        <img src={drop} alt="drop-down"/>
+      </button>
+    </div>
+  )
 }
 
 export default NavBar;
